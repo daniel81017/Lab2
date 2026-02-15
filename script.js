5e728f2e2bb0198876ef02bd23f0e4c8c5ce69fd
Daniel <daniel81017@gmail.com>
2026-02-15 11:40:28 -0500

    navbar and subsequent edits

--- script.js
+++ script.js
@@ -31,38 +31,6 @@ map.on('load', () => {
     // console.log("aaa: " + map.getFilter("walthamstow-lines"));
 });
 
-const map2 = new mapboxgl.Map(
-    {
-        container: 'main-map2',
-        style: 'mapbox://styles/mapbox/standard',
-        center: [-0.01624, 51.58548],
-        zoom: 12,
-    }
-);
-
-map2.on('load', () => {
-    // console.log("--------");
-    map2.addSource('walthamstow-data', {
-        type: 'geojson',
-        data: "https://raw.githubusercontent.com/daniel81017/Lab2/refs/heads/main/walthamstowpoints.geojson"
-        }
-    );
-
-    map2.addLayer({
-        'id': 'walthamstow-points',
-        'type': 'circle',
-        'source': 'walthamstow-data',
-        'paint': {
-            'circle-width': 3,
-            'circle-color': '#6e2222'
-        },
-        // 'filter': ['get'['==', 'type', 'LineString']], // THIS TYPE/LINE STRING IS A PROBLEM
-    });
-    // map.setFilter('walthamstow-lines', ['==', ['get', 'type'], 'LineString']);
-    // console.log("aaa: " + map.getFilter("walthamstow-lines"));
-});
-
-
 // JUST IN CASE
 // map.addLayer({
 //     'id': 'walthamstow-lines',
---
+++ walthamstowsurroundings.js
@@ -0,0 +1,32 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiZGFuaWVsODEwMTciLCJhIjoiY21rZWI2eGg4MDU5NjNscHdxbjhkMTNmciJ9.jdsMukp7zHz3llySNBJs0A"
+
+const map = new mapboxgl.Map(
+    {
+        container: 'main-map1',
+        style: 'mapbox://styles/mapbox/standard',
+        center: [-0.01624, 51.58548],
+        zoom: 12,
+    }
+);
+
+map.on('load', () => {
+    // console.log("--------");
+    map.addSource('walthamstow-data', {
+        type: 'geojson',
+        data: "https://raw.githubusercontent.com/daniel81017/Lab2/refs/heads/main/walthamstowlines.geojson"
+        }
+    );
+
+    map.addLayer({
+        'id': 'walthamstow-lines',
+        'type': 'line',
+        'source': 'walthamstow-data',
+        'paint': {
+            'line-width': 3,
+            'line-color': '#6e2222'
+        },
+        // 'filter': ['get'['==', 'type', 'LineString']], // THIS TYPE/LINE STRING IS A PROBLEM
+    });
+    // map.setFilter('walthamstow-lines', ['==', ['get', 'type'], 'LineString']);
+    // console.log("aaa: " + map.getFilter("walthamstow-lines"));
+});
